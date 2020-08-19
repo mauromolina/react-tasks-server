58acbeda9f6fc5e2c58b2f6ea452e25460f38cef
Mauro <molinamauro2@gmail.com>
2020-08-18 22:47:42 -0300

    fixed users model and create user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,13 @@
+const User = require('../models/User');
 
-
-exports.createUser = (req, res) => {
-    console.log(req.body);
+exports.createUser = async (req, res) => {
+    try {
+        let user;
+        user = new User(req.body);
+        await user.save();
+        res.send('Usuario creado correctamente!')
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
 }
--- models/User.js
+++ models/User.js
@@ -23,4 +23,4 @@ const UserSchema = mongoose.Schema({
     }
 })
 
-module.exports = mongoose.Model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
