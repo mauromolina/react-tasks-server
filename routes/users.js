0db8762dbffeaa26d182716898f5f49ffa727dae
Mauro <molinamauro2@gmail.com>
2020-08-19 04:08:01 -0300

    added the jwt for the control of sessions, hashes in passwd and validations to the API

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { check } = require('express-validator');
 
 router.post('/',
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'Agrega un email valido').isEmail(),
+        check('password', 'La contraseña debe tener mínimo 6 caractéres').isLength({min: 6})
+    ],
     userController.createUser
     );
 
